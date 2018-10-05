1e59b29676b280cd47343a6bdbb61da863159f83
EdGHarahash <forborder2@yandex.ru>
2018-10-05 20:09:05 +0300

    all bounded, but with holes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :things
   resources :categories
   post "little_things/edit", to: "little_things#edit"
+  post "little_things/create", to: "little_things#create"
   root to: "home#index"
 end
